6c2bfe34ae980a02941e718513c383cc1c3d6ddf
Wasi Ullah <mwasiullah9@gmail.com>
2025-01-17 19:25:14 +0100

    develping_gemma

---
+++ modelling_gemma.py
@@ -0,0 +1,66 @@
+import math
+import torch
+from torch import nn
+from typing import dict, Optional, List, Tuple
+from torch.nn import CrossEntropyLoss
+from modeling_siglip import SiglipVisionConfig, SiglipVisionModel
+
+class KVCache():
+    def __init__(self) -> None:
+        self.key_cache: List[torch.Tensor] = []
+        self.value_cache: List[torch.Tensor] = []
+    
+    def num_items(self) -> int:
+        if len(self.key_cache) == 0:
+            return 0
+        else:
+            return self.key_cache[0].shape[-2]
+        
+    def update(
+            self,
+            key_states: torch.Tensor,
+            value_states: torch.Tensor,
+            layer_idx: int,
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        if len(self.key_cache) <= layer_idx:
+            self.key_cache.append(key_states)
+            self.value_cache.append(value_states)
+        else:
+            self.key_cache[layer_idx] = torch.cat([self.key_cache[layer_idx], key_states], dim=-2)
+            self.value_cache[layer_idx] = torch.cat([self.value_cache[layer_idx], value_states], dim=-2)
+        
+        return self.key_cache[layer_idx], self.value_cache[layer_idx]
+    
+class GemmaConfig():
+    def __init__(
+            self,
+            vocab_size,
+            hidden_size,
+            intermediate_size,
+            num_hidden_layers,
+            num_attention_layers,
+            num_key_value_heads,
+            head_dim=256,
+            max_position_embeddings=8192,
+            rms_norm_eps=1e-6,
+            rope_theta=10000.0,
+            attention_bias=False,
+            attention_dropout=0.0,
+            pad_token_id=None,
+            **kwargs
+    ):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.max_position_embeddings = max_position_embeddings
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_layers = num_attention_layers
+        self.head_dim = head_dim
+        self.num_key_value_heads = num_key_value_heads
+        self.rms_norm_eps = rms_norm_eps
+        self.rope_theta = rope_theta
+        self.attention_bias = attention_bias
+        self.attention_dropout = attention_dropout
+        self.pad_token_id = pad_token_id
+
